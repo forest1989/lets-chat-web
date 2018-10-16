7499c061c91188763f0968d11fd07c1c487a0ef6
mahe123 <345341339@qq.com>
2018-10-16 15:12:02 +0800

    完善数据库初始脚本

--- db/0构建表空间用户_windows.sql
+++ db/0构建表空间用户_windows.sql
@@ -1,6 +1,12 @@
 --1 D:\oracle\Administrator\oradata\orcl
---1
---创建表空间脚本
+--该脚本需在数据库管理员权限的用户下操作
+--1、创建临时表空间
+create temporary tablespace WEBTEMP  
+tempfile 'D:\app\mahe\oradata\orcl\WEBTEMP.DBF' 
+size 50m  
+autoextend on  next 50m maxsize 20480m  
+extent management local; 
+--2、创建表空间脚本
 create tablespace WEBDATA  
 logging  
 datafile 'D:\app\mahe\oradata\orcl\WEBDATA.DBF' 
@@ -9,11 +15,14 @@ autoextend on
 next 50m maxsize 20480m  
 extent management local;
 
---创建用户脚本
+--3、创建用户脚本
 create user jeesite identified by jeesite
 default tablespace WEBDATA  
-temporary tablespace TEMP;
+temporary tablespace WEBTEMP;
+
+--4、给用户授予权限
 grant connect,resource,dba to jeesite;
+
 
 
 
