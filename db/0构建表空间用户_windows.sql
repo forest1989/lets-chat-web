382d4fb93b5d257190c3d84ff728326d57dcde4b
mahe123 <345341339@qq.com>
2018-10-18 17:43:07 +0800

    服务器数据库表空间及用户新建

--- db/0构建表空间用户_windows.sql
+++ db/0构建表空间用户_windows.sql
@@ -1,27 +1,33 @@
 --1 D:\oracle\Administrator\oradata\orcl
 --该脚本需在数据库管理员权限的用户下操作
+--删除用户，及级联关系也删除掉
+drop user letschat cascade;
+--删除表空间，及对应的表空间文件也删除掉
+drop tablespace WEBTEMP including contents and datafiles cascade constraint;
+drop tablespace WEBDATA including contents and datafiles cascade constraint;
+
 --1、创建临时表空间
 create temporary tablespace WEBTEMP  
-tempfile 'D:\app\mahe\oradata\orcl\WEBTEMP.DBF' 
+tempfile 'C:\app\Administrator\oradata\orcl\WEBTEMP.DBF' 
 size 50m  
 autoextend on  next 50m maxsize 20480m  
 extent management local; 
 --2、创建表空间脚本
 create tablespace WEBDATA  
 logging  
-datafile 'D:\app\mahe\oradata\orcl\WEBDATA.DBF' 
+datafile 'C:\app\Administrator\oradata\orcl\WEBDATA.DBF' 
 size 50m  
 autoextend on  
 next 50m maxsize 20480m  
 extent management local;
 
 --3、创建用户脚本
-create user jeesite identified by jeesite
+create user letschat identified by letschat
 default tablespace WEBDATA  
 temporary tablespace WEBTEMP;
 
 --4、给用户授予权限
-grant connect,resource,dba to jeesite;
+grant connect,resource,dba to letschat;
 
 
 
