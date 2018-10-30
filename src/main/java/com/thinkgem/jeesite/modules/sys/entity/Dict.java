803d877ce391d1736ffc452b349ac2c3a6b8b14f
tyg <tyg@tygPC>
2018-10-30 16:02:31 +0800

    修改字典url

--- src/main/java/com/thinkgem/jeesite/modules/sys/entity/Dict.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/entity/Dict.java
@@ -24,6 +24,7 @@ public class Dict extends DataEntity<Dict> {
 	private String description;// 描述
 	private Integer sort;	// 排序
 	private String parentId;//父Id
+	private String dicUrl;//字典图片地址
 
 	public Dict() {
 		super();
@@ -99,4 +100,19 @@ public class Dict extends DataEntity<Dict> {
 	public String toString() {
 		return label;
 	}
+
+	/**
+	 * @return the dicUrl
+	 */
+	public String getDicUrl() {
+		return dicUrl;
+	}
+
+	/**
+	 * @param dicUrl the dicUrl to set
+	 */
+	public void setDicUrl(String dicUrl) {
+		this.dicUrl = dicUrl;
+	}
+	
 }
