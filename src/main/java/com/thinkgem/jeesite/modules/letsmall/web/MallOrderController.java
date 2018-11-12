7eec8f40282b9e6ad6c2ca9748e8af1d200d3503
forest <forest@192.168.226.1>
2018-11-12 10:01:41 +0800

    1

--- src/main/java/com/thinkgem/jeesite/modules/letsmall/web/MallOrderController.java
+++ src/main/java/com/thinkgem/jeesite/modules/letsmall/web/MallOrderController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
