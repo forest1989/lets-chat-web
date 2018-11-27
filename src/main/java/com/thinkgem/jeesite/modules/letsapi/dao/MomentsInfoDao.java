4e86b8c8653bb24edd5ae8daa295af86baec2890
adminZHAI <24836164@qq.com>
2018-11-27 15:12:35 +0800

    更新 MomentsInfoDao.java

--- src/main/java/com/thinkgem/jeesite/modules/letsapi/dao/MomentsInfoDao.java
+++ src/main/java/com/thinkgem/jeesite/modules/letsapi/dao/MomentsInfoDao.java
@@ -3,6 +3,8 @@
  */
 package com.thinkgem.jeesite.modules.letsapi.dao;
 
+import java.util.List;
+
 import com.thinkgem.jeesite.common.persistence.CrudDao;
 import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
 import com.thinkgem.jeesite.modules.letsapi.entity.MomentsInfo;
@@ -15,4 +17,27 @@ import com.thinkgem.jeesite.modules.letsapi.entity.MomentsInfo;
 @MyBatisDao
 public interface MomentsInfoDao extends CrudDao<MomentsInfo> {
 	
+	/**
+	 * @param 朋友圈查询 之：只给状态0 查询最新十条朋友圈，刚进来朋友圈
+	 * @return
+	 */
+	List<MomentsInfo> findMoments0(MomentsInfo momentsInfo);
+	
+	/**
+	 * @param 朋友圈查询 之：有时间&有查询状态1----下拉获取时间轴往上所有
+	 * @return
+	 */
+	List<MomentsInfo> findMoments1(MomentsInfo momentsInfo);
+	
+	/**
+	 * @param 朋友圈查询 之：有时间&有查询状态2---上滑 查询下一页
+	 * @return
+	 */
+	List<MomentsInfo> findMoments2(MomentsInfo momentsInfo);
+	
+	/**
+	 * @param 朋友圈查询 之：查询我的相册
+	 * @return
+	 */
+	List<MomentsInfo> findMyMoments(MomentsInfo momentsInfo);
 }
